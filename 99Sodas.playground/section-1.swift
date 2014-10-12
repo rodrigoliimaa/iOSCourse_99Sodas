4f19bbb4264145cafa35441ae2edf5a635eb7cfd
Rodrigo Lima <rodrigosan.lima@gmail.com>
2014-10-12 00:52:36 -0300

    99Sodas Challenge

---
+++ 99Sodas.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+for (var i = 99; i >= 0; i--) {
+    if i == 0 {
+        println("No more bottles of soda on the wall")
+    } else {
+        println("\(i) bottles of soda on the wall")
+    }
+}
